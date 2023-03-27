8bdb066536677dfdab44ce0f3133e4f74cb729dd
yuyu <hcsd7b34@gmail.com>
2023-03-27 18:37:10 +0900

    Add: builtinの補完を実装

---
+++ src/extensions.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+
+class EdmlCompletionItemProvider {
+	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+		let completionList: vscode.CompletionItem[] = [];
+
+		let builtins = [];
+		builtins.push(...[
+			'import', 'boolean', 'number', 'string', 'date', 'regex', 'error', 'range',
+			'now', 'utc', 'setDate', 'setTime', 'setYear', 'setMonth', 'setDay', 'setHour', 'setMinute', 'setSecond', 'setMillisecond',
+			'addDateTime', 'addDate', 'addTime', 'addYear', 'addMonth', 'addDay', 'addHour', 'addMinute', 'addSecond', 'addMillisecond', 'formatDate',
+			'match', 'matchGroup', 'test', 'search', 'replace', 'split',
+			'abs', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atanh', 'atan2', 'cbrt', 'ceil', 'cos', 'cosh', 'exp',
+			'floor', 'hypot', 'log', 'log10', 'log2', 'random', 'round', 'sign', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc',
+			'print', 'printTable'
+		].map(i => new vscode.CompletionItem(i, vscode.CompletionItemKind.Function)));
+		builtins.push(...[
+			'const'
+		].map(i => new vscode.CompletionItem(i, vscode.CompletionItemKind.Constant)))
+
+		if (context.triggerCharacter == null) {
+			completionList.push(...builtins);
+			completionList.push(...[
+				'builtin', 'global'
+			].map(i => new vscode.CompletionItem(i, vscode.CompletionItemKind.Keyword)))
+		} else if (context.triggerCharacter == '.') {
+			let parentName = document.getText(new vscode.Range(new vscode.Position(0, 0), position))
+				.match(/([0-9a-zA-Z_.\s]+)\.$/)?.[1].replace(/\s/g, '')
+			
+			if (parentName == 'builtin') completionList.push(...builtins);
+			else if (parentName == 'const' || parentName == 'builtin.const') {
+				completionList.push(...[
+					'e', 'pi', 'epsilon', 'minValue', 'maxValue'
+				].map(i => new vscode.CompletionItem(i, vscode.CompletionItemKind.Constant)))
+			}
+		}
+
+		return completionList;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider('edml', new EdmlCompletionItemProvider(), '.')
+	);
+}
+
+export function deactivate() {}
